6dd644e213c7308e881d664008665da5ddc09654
syahraazly <syahrazulyashaniamaghfiroh@gmail.com>
2023-12-21 09:41:32 +0700

    fix: function calculate & style

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,21 @@
 <template>
-    <v-app style="background-color: #263238;">
-        <v-container>
-            <h1>Age Calculator</h1>
-            <v-card class="container">
+    <v-app style="background-color: #ECEFF1">
+        <v-container class="container">
+            <v-card>
+                <h1>Age Calculator</h1>
                 <div class="calculate">
                     <v-row>
                         <v-col cols="12" md="8">
                             <v-text-field v-model="inputDate" variant="outlined" type="date"></v-text-field>
                         </v-col>
                         <v-col cols="12" md="4">
-                            <v-btn @click="calculateAge">Calculate</v-btn>
+                            <v-btn @click="calculateAge()" variant="text" color="blue">Calculate</v-btn>
                         </v-col>
-                        <h2 v-if="ageCalculated">Umur kamu sekarang adalah {{ y3 }} tahun, {{ m3 }} bulan, {{ d3 }} hari
-                        </h2>
+                        <h4 v-if="ageCalculated">Umur kamu sekarang adalah <span>{{ y3 }}</span> tahun, <span>{{ m3
+                        }}</span> bulan, <span>{{ d3 }}</span> hari
+                        </h4>
                     </v-row>
+                    <v-btn @click="clear()" style="margin-top: 3rem;" variant="text" color="red">Clear</v-btn>
                 </div>
             </v-card>
         </v-container>
@@ -45,54 +47,85 @@ export default {
             let m2 = today.getMonth();
             let y2 = today.getFullYear();
 
-            let d3, m3, y3;
-
-            y3 = y2 - y1;
+            this.y3 = y2 - y1;
 
             if (m2 >= m1) {
-                m3 = m2 - m1;
+                this.m3 = m2 - m1;
             } else {
-                y3--;
-                m3 = 12 + m2 - m1;
+                this.y3--;
+                this.m3 = 12 + m2 - m1;
             }
 
             if (d2 >= d1) {
-                d3 = d2 - d1;
+                this.d3 = d2 - d1;
             } else {
-                m3--;
-                d3 = getDaysInMonth(y1, m1) + d2 - d1;
+                this.m3--;
+                this.d3 = this.getDaysInMonth(y1, m1) + d2 - d1;
             }
 
-            if (m3 < 0) {
-                m3 = 11;
-                y3--;
+            if (this.m3 < 0) {
+                this.m3 = 11;
+                this.y3--;
             }
+
+            this.ageCalculated = true;
         },
         getDaysInMonth(year, month) {
             return 32 - new Date(year, month, 0).getDate();
+        },
+        clear() {
+            this.inputDate = new Date().toISOString().substr(0, 10);
+            this.y3 = null;
+            this.m3 = null;
+            this.d3 = null;
+            this.ageCalculated = false;
         }
     }
-
-
 }
 </script>
 
 <style>
 .container {
     margin-top: 3rem;
-    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+}
+
+.v-card {
+    max-width: 600px;
+    /* Lebar maksimal untuk card */
+    width: 100%;
+    /* Lebar full pada ukuran layar kecil */
 }
 
 h1 {
     margin-top: 2rem;
-    margin-bottom: 2rem;
-    color: #ECEFF1;
-    text-align: center;
+    /* margin-bottom: 1rem; */
+    /* text-align: center; */
+    padding-left: 3rem;
+    font-weight: bold;
+}
+
+h4 {
+    padding-left: 1rem;
+}
+
+h4 span {
+    color: #5E747F;
 }
 
 .calculate {
-    padding: 3rem;
+    padding: 1rem;
     margin-top: 2rem;
-    margin-bottom: 2rem;
+    /* margin-bottom: 2rem; */
+    /* text-align: center; */
+}
+
+@media (min-width: 600px) {
+    .calculate {
+        padding: 3rem;
+    }
 }
 </style>
